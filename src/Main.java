007ef1f33e1bee3ed668bb5c467bb97ba82ed78e
Vladimir Alferyev <vladimir.a.alferyev@gmail.com>
2021-06-29 21:56:42 +0500

    Fail cart

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "4485604470539276";
+        String number = "4485880578131742859";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
